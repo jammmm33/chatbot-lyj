f49097ace0f675af06cf057360ab49e34d2d67f9
jammmm33 <jammmm33@gmail.com>
2025-06-13 16:44:26 +0900

    feat: build full-featured RAG chatbot for national policy guide

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,21 @@
+import uuid
 import streamlit as st
+from llm import get_ai_message
 
-st.set_page_config(page_title='아직 생각중인 챗봇', page_icon='😎')
-st.title('아직 생각중인 챗봇😎')
+st.set_page_config(page_title='청년수당 안내문 챗', page_icon='😎')
+st.title('청년수당 안내문 챗봇😎')
+st.markdown("---")
+
+query_params = st.query_params
+
+if 'session_id' in query_params:
+    session_id = query_params['session_id']
+else:
+    session_id = str(uuid.uuid4())
+    st.query_params.update({'session_id': session_id})
+
+if 'session_id' not in st.session_state:
+    st.session_state['session_id'] = session_id
 
 if 'message_list' not in st.session_state:
     st.session_state.message_list = []
@@ -17,7 +31,13 @@ if user_question := st.chat_input(placeholder='여기에 질문을 입력해주�
         st.write(user_question)
     st.session_state.message_list.append({'role':'user', 'content': user_question})
 
-with st.chat_message('ai'):
-    st.write('여기는 AI 메시지')
-st.session_state.message_list.append({'role':'ai', 'content':'여기는 AI 메시지'})
+    with st.spinner('답변을 생성하는 중입니다.'):
+        ai_message = get_ai_message(user_question)
+
+    with st.chat_message('ai'):
+        ai_message = st.write_stream(ai_message)
+    st.session_state.message_list.append({'role':'ai', 'content':ai_message})
+
+st.info("청년수당에 관한 궁금한 내용을 아래 채팅창에 입력해보세요!", icon="💬")
+
     
---
+++ llm.py
@@ -0,0 +1,125 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.chains import (create_history_aware_retriever,
+                              create_retrieval_chain)
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+# 환경 변수 로딩
+load_dotenv()
+
+
+def get_llm(model='gpt-4o'):
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+def get_database():
+
+    PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+    embedding = OpenAIEmbeddings(model="text-embedding-3-large")
+    index_name = "chungnun"
+
+    # 벡터스토어에 인엑스 가져오기
+    database = PineconeVectorStore.from_existing_index(
+        embedding=embedding,
+        index_name=index_name
+    )
+    return database
+
+
+store = {}
+
+def get_seesion_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+def get_history_retriever(llm, retriever):
+    contextualize_q_system_prompt = (
+    '''채팅 히스토리와 가장 최근 사용자의 질문이 주어졌을 때,  
+    그 질문이 이전 대화의 문맥을 참고할 수 있다는 점을 고려하여,  
+    이전 히스토리 없이도 이해 가능한 독립적인 질문으로 바꿔주세요.  
+    질문에 답변하지는 마세요.  
+    필요하다면 문장을 재작성하고, 그렇지 않으면 그대로 반환하세요.'''
+    )
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+    )
+
+    history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+    )
+
+    return history_aware_retriever
+
+
+def get_qa_prompt():
+    system_prompt = (
+         '''
+    -당신은 청년수당에 대한 안내문 입니다. 
+    -[context]를 참고하여 사용자의 질문에 답변하세요.
+    -청년수당 안내문에 대한 정보 이외에는 '답변을 할 수 없습니다.' 로 답하세요.
+    -필요하다면 문서에서 직접 인용하거나 요약된 내용을 덧붙이세요.
+    -사용자가 '알려주세요', '설명해주세요'  질문에도 답변하세요.
+    -항목별로 표시해서 답변해주세요.
+    
+    [context]
+    {context} 
+    '''
+    )
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    return qa_prompt
+
+
+def build_chain():
+    llm = get_llm()
+
+    database = get_database()
+    retriever = database.as_retriever()
+
+    history_aware_retriever = get_history_retriever(llm, retriever)
+    qa_prompt = get_qa_prompt()
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_seesion_history,
+        input_messages_key='input',
+         history_messages_key="chat_history",
+        output_messages_key="answer",
+        ).pick('answer')
+    
+    return conversational_rag_chain
+
+
+def get_ai_message(user_message, session_id = 'default'):
+    qa_chain = build_chain()
+
+    ai_message = qa_chain.stream(
+        {'input': user_message},
+        config={"configurable": {"session_id": session_id}}
+    )
+
+    return ai_message
